a49cb2ba7c9f3e49aad31d142804e2bad124c448
Ankit-saha1606 <ankitsaha@kgpian.iitkgp.ac.in>
2025-07-01 01:05:46 +0530

    Update http.cpp

--- src/http.cpp
+++ src/http.cpp
@@ -1,9 +1,5 @@
 /**
- * min2phaseCXX Copyright (C) 2022 Borgo Federico
- * This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
- * This is free software, and you are welcome to redistribute it
- * under certain conditions; type `show c' for details.
- *
+ 
  * This file contains the implementation of all the function
  * used for the remote server solver.
  */
