3c6a52c4d2485bce915554b07bdbd3df006eca88
Ankit-saha1606 <ankitsaha@kgpian.iitkgp.ac.in>
2025-07-01 01:01:59 +0530

    Update tests.cpp

--- examples/tests.cpp
+++ examples/tests.cpp
@@ -1,12 +1,3 @@
-/**
- * min2phaseCXX Copyright (C) 2022 Borgo Federico
- * This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
- * This is free software, and you are welcome to redistribute it
- * under certain conditions; type `show c' for details.
- *
- * This file contains the tests for the algorithm.
- */
-
 #include <min2phase/tools.h>
 
 int main(int argc, char* argv[]){
